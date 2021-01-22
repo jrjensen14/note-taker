a036f38e8a9ecab56ac614ff2bb4278df0b0ba9d
JRJ <jjsocc171@msn.com>
2021-01-21 20:53:31 -0600

    routes set up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,21 @@
 const router = require('express').Router;
-const store = require('../db/store');
+const notesData = require('../db/store');
+
+//API ROUTES: 
+// GET /api/notes should read the db.json file 
+// return all saved notes as JSON.
+router.get('/notes', (req, res) => {
+    res.json(notesData);
+});
+
+// POST /api/notes should receive a new note to save on the request body, 
+// add it to the db.json file, 
+// and then return the new note to the client.
+// You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+router.post('/notes', (req, res) => {
+    req.body = notesData.length.toString();
+
+    res.json(notesData);
+})
+
+module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,18 @@
 const path = require('path');
 const router = require('express').Router;
 
+//HTML Routes needed:
+// GET /notes(should return notes.html file)
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+// GET * (should retrun index.html file)
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+
+
+
+module.exports = router;
